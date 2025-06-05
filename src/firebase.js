696bb58deeccb169b649aa55b8100733f3687416
Nana Magalashvili <nanamagalashvili@lp-0721.local>
2025-06-05 16:40:07 +0400

    added storage

--- src/firebase.js
+++ src/firebase.js
@@ -1,12 +1,13 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
-import { getStorage } from 'firebase/storage'; 
+import { getStorage } from 'firebase/storage';
+
 
 const firebaseConfig = {
   apiKey: "AIzaSyD06lICMUULN4ByLgQIZEXv89fXC4FHuv8",
   authDomain: "sweet-lab-2fda2.firebaseapp.com",
   projectId: "sweet-lab-2fda2",
-  storageBucket: "sweet-lab-2fda2.firebaseapp.com",
+  storageBucket: 'sweet-lab-bucket',
   messagingSenderId: "523115946393",
   appId: "1:523115946393:web:63078fabcb14161192f308",
   measurementId: "G-JM6ECM7D8L"
