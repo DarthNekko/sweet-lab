84ed5291d4a2a66dc1a6d25228bb7c78722cda33
Nana Magalashvili <nanamagalashvili@lp-0721.local>
2025-06-05 15:09:39 +0400

    route

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './Header';
 import Hero from './Hero';
 import Menu from './Menu';
 import ContactModal from './ContactModal';
+import AdminPanel from './AdminPanel';
 import './Footer.css';
 import './global.css';
 
-
-
 function App() {
   return (
-    <div>
-      <Header />
-      <Hero />
-      <Menu />
-      <ContactModal />
-      <footer className="site-footer">
-  <span>© 2025 Sweet Lab. All rights reserved.</span>
-</footer>
-    </div>
+    <Router>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <>
+              <Header />
+              <Hero />
+              <Menu />
+              <ContactModal />
+              <footer className="site-footer">
+                <span>© 2025 Sweet Lab. All rights reserved.</span>
+              </footer>
+            </>
+          }
+        />
+        <Route path="/admin" element={<AdminPanel />} /> {/* admin route */}
+      </Routes>
+    </Router>
   );
 }
-
 
 export default App;
